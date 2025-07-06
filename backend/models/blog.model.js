36aa434ecff8223599a84a41585f0c5363164f14
Jatinmulchandani <Jatinmulchandani18@gmail.com>
2025-07-06 17:17:47 +0530

    blog models file

---
+++ backend/models/blog.model.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 100,
+    },
+    content: {
+      type: String,
+      required: true,
+      minLength: 10,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      trim: true,
+    },
+    likes: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        timestamp: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    dislikes: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        timestamp: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model("Blog", blogSchema);
+export default Blog;
